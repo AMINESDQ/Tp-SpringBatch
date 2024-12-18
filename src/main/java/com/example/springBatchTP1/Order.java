b3400f297e6a63ae746550b4cb1e49d3a4874d38
Mohamed Amine Sadiq <aminesadiq@gmail.com>
2024-12-18 11:36:28 +0100

    first commit

---
+++ src/main/java/com/example/springBatchTP1/Order.java
@@ -0,0 +1,4 @@
+package com.example.springBatchTP1;
+
+public record Order(String orderId,String customerName,double amount) {
+}
---
+++ src/main/java/com/example/springBatchTP1/SpringBatchTp1Application.java
@@ -0,0 +1,13 @@
+package com.example.springBatchTP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTp1Application.class, args);
+	}
+
+}
